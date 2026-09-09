57481ecae67e01d5ae8f450e458926c7af8739e4
Boram-Hwang <95403102+Boram-Hwang@users.noreply.github.com>
2026-09-09 16:09:18 +0900

    [feat] 그룹 챌린지 시즌 조회 추가 (#58)
    * feat: 그룹 챌린지 시즌 조회 추가
    
    * fix: 코드 스타일 변경

---
+++ back/src/main/java/com/gommit/domain/group/dto/response/SeasonSummary.java
@@ -0,0 +1,5 @@
+package com.gommit.domain.group.dto.response;
+
+import com.gommit.domain.challenge.entity.ChallengeStatus;
+
+public record SeasonSummary(Long id, int seqNo, ChallengeStatus status) {}
